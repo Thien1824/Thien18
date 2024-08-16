791f144b682fea099a6cb2dc1b3d8c53d63e3f2f
THIEN <23670311.thien@student.iuh.edu.vn>
2024-08-16 17:11:49 +0700

    FirstPrj

---
+++ src/welcome.java
@@ -0,0 +1,6 @@
+public class welcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello Ngo Phuoc Thien!\n");
+        System.out.println("DHKTPM19A");
+        }
+    }
